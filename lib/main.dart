4ea34e1d6fd61c850cffd3a17d2ce858c30417de
edgvac2298 <edgardovac2298@gmail.com>
2022-04-16 18:02:23 -0500

    Adding icons to app

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart'; 
+
 void main() {
   runApp(const MyApp());
 }
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(), 
+      home: RandomWords(),
     );
   }
+  // #enddocregion build
 }
+// #enddocregion MyApp
 
+// #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
-      final _suggestions = <WordPair>[];
-      final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
-@override
+  // #docregion RWS-build
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -40,19 +48,25 @@ class _RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
-          // #docregion listTile
+          final alreadySaved = _saved.contains(_suggestions[index]);
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
             ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
           );
-          // #enddocregion listTile
+          // #enddocregion 
         },
       ),
       // #enddocregion itemBuilder
     );
   }
+  
   // #enddocregion RWS-build
   // #docregion RWS-var
 }
